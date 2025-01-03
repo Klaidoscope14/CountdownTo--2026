9b9c4394cdf9efe0cb28b66cb2ffd9eb7b719254
Klaidoscope14 <saagarchaitanya80@gmail.com>
2025-01-03 20:42:08 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Countdown from './Countdown2026';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <React.StrictMode>
+    <Countdown/>
+  </React.StrictMode>
+);
